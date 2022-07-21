dc649a0bef017bb77dca825b440d31d9f4d0fbe7
Rajdeep Anand <v-raanand@microsoft.com>
2022-07-21 14:32:05 +0530

    Change in loop logic, comments

--- rooms-quickstart/rooms.py
+++ rooms-quickstart/rooms.py
@@ -1,6 +1,4 @@
-import sys
-
-from datetime import datetime
+from datetime import datetime, timezone
 from dateutil.relativedelta import relativedelta
 from azure.core.exceptions import HttpResponseError
 from azure.communication.rooms import (
@@ -25,7 +23,7 @@ class RoomsQuickstart(object):
     
     def create_room(self):
         try:
-            valid_from = datetime.now()
+            valid_from = datetime.now(timezone.utc)
             valid_until = valid_from + relativedelta(months=+1,days=+20)
             participants = []
             participants.append(RoomParticipant(CommunicationUserIdentifier(self.participant1)))
@@ -37,7 +35,7 @@ class RoomsQuickstart(object):
             print(ex)
 
     def update_room(self, room_id:str):
-        valid_from = datetime.now()
+        valid_from = datetime.now(timezone.utc)
         valid_until = valid_from + relativedelta(months=+1,days=+20)
         try:
             update_room_response = self.rooms_client.update_room(room_id=room_id, valid_from=valid_from, valid_until=valid_until)
@@ -54,41 +52,44 @@ class RoomsQuickstart(object):
     def print_room(self, response):
         print("room Id: " + response.id  + "\n" + 
         "created date time: " + str(response.created_date_time) + "\n"  + 
-        "valid_from: " + str(response.valid_from) + "\n" + "valid_until: ", str(response.valid_until))
+        "valid_from: " + str(response.valid_from) + "\n" + "valid_until: ", str(response.valid_until) + "\n" +
+        "Participants : \n" )
+        for i in range(len(response.participants)):
+            print(str(i+1) + ' -    ' + response.participants[i].communication_identifier.properties['id'])
 
     def get_participants_in_room(self, room_id:str):
         room = self.rooms_client.get_room(room_id=room_id)
         participants = room.participants
-        participants_list = list(participants)
-        print('\nCurrent room participants : \n' + 'S.no. - '+ 'User Id')
-        for p in participants_list:
-            print(str(participants_list.index(p)) + ' -     ' + str(p.communication_identifier.properties['id']) )
+        print('\nCurrent room participants : \n' + 'S.no.  '+ 'User Id')
+        for i in range(len(participants)):
+            print(str(i+1) + ' -    ' + participants[i].communication_identifier.properties['id'])
 
     def add_participant_to_room(self, room_id:str, participants:list):
         try:
-            users = []
-            for p in participants:
-                users.insert(participants.index(p), RoomParticipant(CommunicationUserIdentifier(p)))
-            self.rooms_client.add_participants(room_id=room_id, participants=users)
+            participants_to_add = []
+            for i in range(len(participants)):
+                participants_to_add.insert(i, RoomParticipant(CommunicationUserIdentifier(participants[i])))
+
+            self.rooms_client.add_participants(room_id=room_id, participants=participants_to_add)
             print('\nAdded ' + str(len(participants)) + ' new participants to the Room : ' + str(room_id))
         except Exception as ex:
             print(ex)
 
     def remove_participant_from_room(self, room_id:str, participants:list):
         try:
-            users = []
-            for p in participants:
-                users.insert(participants.index(p), CommunicationUserIdentifier(p))
-            self.rooms_client.remove_participants(room_id=room_id, communication_identifiers=users)
-            print('n\Removed ' + str(len(participants)) + ' participants from the Room : ' + str(room_id))
+            participants_to_remove = []
+            for i in range(len(participants)):
+                participants_to_remove.insert(i, CommunicationUserIdentifier(participants[i]))
+            self.rooms_client.remove_participants(room_id=room_id, communication_identifiers=participants_to_remove)
+            print('\nRemoved ' + str(len(participants)) + ' participants from the Room : ' + str(room_id))
         except Exception as ex:
             print(ex)
 
 if __name__ == '__main__':
+    print('-----------------Starting room operations of create --> update --> add --> get --> delete-----------------------')
+
     rooms = RoomsQuickstart()
     rooms.setUp()
-    print('-----------------Starting room operations of create --> update --> add --> get --> delete-----------------------')
-    
     rooms.create_room()
     rooms.update_room(room_id=rooms.roomsCollection[0])
     rooms.add_participant_to_room(rooms.roomsCollection[0], [rooms.participant2, rooms.participant3, rooms.participant4]  )
