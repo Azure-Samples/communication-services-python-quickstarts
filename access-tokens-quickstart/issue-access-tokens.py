ed7a7263fa543ae3c75982ce4911752eb7af328d
Aigerim Beishenbekova <aigerimb@microsoft.com>
2022-10-10 11:32:28 +0200

    fixed PR comments

--- access-tokens-quickstart/issue-access-tokens.py
+++ access-tokens-quickstart/issue-access-tokens.py
@@ -18,18 +18,16 @@ try:
 	#Store the identity to issue access tokens later
 	existingIdentity = identity	
 
-	# Issue an access token with validity of 24 hours and the "voip" scope for an identity
+	# Issue an access token with a validity of 24 hours and the "voip" scope for an identity
 	token_result = client.get_token(identity, ["voip"])
 	print("\nIssued an access token with 'voip' scope that expires at " + token_result.expires_on + ":")
 	print(token_result.token)
  
-	# Issue an access token with validity of an hour and the "voip" scope for an identity
+	# Issue an access token with a validity of an hour and the "voip" scope for an identity
 	token_expires_in = timedelta(hours=1)
 	token_result = client.get_token(identity, ["voip"], token_expires_in=token_expires_in)
-	print("\nIssued an access token with 'voip' scope and custom expiration that expires at " + token_result.expires_on + ":")
-	print(token_result.token)
 	
-	# Create an identity and issue an access token with validity of 24 hours within the same request
+	# Create an identity and issue an access token with a validity of 24 hours within the same request
 	identity_token_result = client.create_user_and_token(["voip"])
 	# Get the token details from the response
 	identity = identity_token_result[0]
@@ -37,16 +35,6 @@ try:
 	expires_on = identity_token_result[1].expires_on
 	print("\nCreated an identity with ID: " + identity.properties['id'])
 	print("\nIssued an access token with 'voip' scope that expires at " + expires_on + ":")
-	print(token)
- 
-	# Create an identity and issue an access token with validity of an hour within the same request
-	identity_token_result = client.create_user_and_token(["voip"], token_expires_in=token_expires_in)
-	# Get the token details from the response
-	identity = identity_token_result[0]
-	token = identity_token_result[1].token
-	expires_on = identity_token_result[1].expires_on
-	print("\nCreated an identity with ID: " + identity.properties['id'])
-	print("\nIssued an access token with 'voip' scope and custom expiration that expires at " + expires_on + ":")
 	print(token)
  	
 	# Refresh access tokens - existingIdentity represents identity of Azure Communication Services stored during identity creation
