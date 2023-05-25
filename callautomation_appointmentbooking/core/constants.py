46f46a6217fc70fbdc72189738c1749dc53de5fd
vivekmore@microsoft.com <vivekmore@microsoft.com>
2023-05-25 19:04:10 +0530

    Call automation appointment booking application

---
+++ callautomation_appointmentbooking/core/constants.py
@@ -0,0 +1,1 @@
+# Event namesCALL_CONNECTED_EVENT = "Microsoft.Communication.CallConnected"PLAY_COMPLETED_EVENT = "Microsoft.Communication.PlayCompleted"RECOGNIZE_COMPLETED_EVENT = "Microsoft.Communication.RecognizeCompleted"RECOGNIZE_FAILED_EVENT = "Microsoft.Communication.RecognizeFailed"
