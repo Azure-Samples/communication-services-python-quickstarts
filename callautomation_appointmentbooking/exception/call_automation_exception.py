f49dba530ecdf53ae6f0b91ec391a56ba80f03fe
vivekmore@microsoft.com <vivekmore@microsoft.com>
2023-05-26 18:53:04 +0530

    Corrected line endings.

--- callautomation_appointmentbooking/exception/call_automation_exception.py
+++ callautomation_appointmentbooking/exception/call_automation_exception.py
@@ -1,1 +1,4 @@
-class CallAutomationException(Exception):    def __init__(self, error_details):        super().__init__(str(error_details))        self.error_details = error_details
+class CallAutomationException(Exception):
+    def __init__(self, error_details):
+        super().__init__(str(error_details))
+        self.error_details = error_details
