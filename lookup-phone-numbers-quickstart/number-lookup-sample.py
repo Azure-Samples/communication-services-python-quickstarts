8e33f6e1b403759927f8446720b7c6907a9ff251
Erica <34174887+ericasp16@users.noreply.github.com>
2024-03-22 11:51:00 -0700

    Update number-lookup-sample.py
    pull the phone number from the command line

--- lookup-phone-numbers-quickstart/number-lookup-sample.py
+++ lookup-phone-numbers-quickstart/number-lookup-sample.py
@@ -1,8 +1,13 @@
 import os
+import sys
 from azure.communication.phonenumbers import PhoneNumbersClient
-
+    
 try:
     print('Azure Communication Services - Number Lookup Quickstart')
+    
+    if len(sys.argv) < 2:
+        sys.exit("Missing a phone number parameter")
+    phoneNumber = sys.argv[1]
     
     # This code retrieves your connection string from an environment variable
     connection_string = os.getenv('COMMUNICATION_SERVICES_CONNECTION_STRING')
@@ -13,14 +18,14 @@ try:
         print(ex)
 
     # Use the free number lookup functionality to get number formatting information
-    formatting_results = phone_numbers_client.search_operator_information("<target-phone-number>")
+    formatting_results = phone_numbers_client.search_operator_information(phoneNumber)
     formatting_info = formatting_results.values[0]
     print(str.format("{0} is formatted {1} internationally, and {2} nationally", formatting_info.phone_number, formatting_info.international_format, formatting_info.national_format))
 
     # Use the paid number lookup functionality to get operator specific details
     # IMPORTANT NOTE: Invoking the method below will incur a charge to your account
     options = { "include_additional_operator_details": True }
-    operator_results = phone_numbers_client.search_operator_information([ "<target-phone-number>" ], options)
+    operator_results = phone_numbers_client.search_operator_information([ phoneNumber ], options)
     operator_information = operator_results.values[0]
 
     number_type = operator_information.number_type if operator_information.number_type else "unknown"
