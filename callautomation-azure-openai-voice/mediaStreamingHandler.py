6d0df5847bc4b04007e7ea96825006cea62ba04c
Fariba Haghbin <fhaghbin@microsoft.com>
2025-06-06 16:49:59 -0400

    Update Call Automation library to GA

--- callautomation-azure-openai-voice/mediaStreamingHandler.py
+++ callautomation-azure-openai-voice/mediaStreamingHandler.py
@@ -4,9 +4,12 @@ from azureOpenAIService import send_audio_to_external_ai
 async def process_websocket_message_async(stream_data):
     try:
         data = json.loads(stream_data)
-        kind = data['kind']
+        kind = data.get('kind')
         if kind == "AudioData":
-            audio_data = data["audioData"]["data"]
-            await send_audio_to_external_ai(audio_data)
+            audio_data_section = data.get("audioData", {})
+            if not audio_data_section.get("silent", True):
+                audio_data = audio_data_section.get("data")
+                await send_audio_to_external_ai(audio_data)
+
     except Exception as e:
         print(f'Error processing WebSocket message: {e}')
